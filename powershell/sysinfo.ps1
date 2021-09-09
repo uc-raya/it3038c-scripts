f1bb40b633aee6daa08e19c028ff61d88b1611f6
uc-raya <raya9@mail.uc.edu>
2021-09-09 15:18:34 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -3,5 +3,11 @@
 }
 
 $IP = getIP
-write-host("This machine's IP is $IP")
-write-host("This machine's IP is {0}" -f $IP)
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$VERSION = $host.Version
+$DATE = Get-Date -Format "dddd MM/dd/yyyy"
+$BODY = "This machine's IP address is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell version $VERSION. Today's date is $DATE"
+## $BODY = This machine's IP is ...., user is $USER, Hostname, PowerShell version, today's date
+
+Send-MailMessage -To "raya9@mail.uc.edu" -From "andrewray317@gmail.com" -Subject "IT3038c Windows SysInfo" -Body $BODY -smtpserver smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
