fcc25db77316a9548f8cf648f1d2ed8c5be0abe2
uc-raya <raya9@mail.uc.edu>
2021-09-26 17:09:27 -0400

    nameage script

---
+++ python/nameage.py
@@ -0,0 +1,9 @@
+print(‘What is your name?’) 
+
+myName = input() 
+
+print(‘Hello, ‘ + myName + ‘. That is a good name. How old are you?’) 
+
+myAge = input() 
+
+print(myAge+’? That’s funny, I'm only a few seconds old.’) 
