a7e56d7b60b255f4a20f63b41189ee897ae85630
uc-raya <raya9@mail.uc.edu>
2021-11-03 15:55:56 -0400

    node

--- node/server.js
+++ node/server.js
@@ -2,7 +2,12 @@ var fs = require("fs")
 var http = require("http");
 var ip = require("ip");
 var os = require("os");
+var totalMemory = require("totalmem");
 
+function getUptime() {
+    serverTime = os.uptime();
+    return serverTime
+}
 var server = http.createServer(function(req, res) {
     if (req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function(err, body) {
@@ -21,8 +26,8 @@ var server = http.createServer(function(req, res) {
                     <body>
                     <p>Hostname: ${myHostName}</p>
                     <p>IP: ${ip.address()}</p>
-                    <p>Server Uptime: </p>
-                    <p>Total Memory: </p>
+                    <p>Server Uptime: ${getUptime()}</p>
+                    <p>Total Memory: ${os.totalmem()}</p>
                     <p>Free Memory: </p>
                     <p>Number of CPUs: </p>
                     </body>
