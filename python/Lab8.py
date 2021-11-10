359ee79d76de1a15a0c30b504b58e0aeb1c4793e
uc-raya <raya9@mail.uc.edu>
2021-11-09 19:39:18 -0500

    Lab 8

--- python/Lab8.py
+++ python/Lab8.py
@@ -1,41 +1,17 @@
-import bs4
-from urllib.request import urlopen
-from bs4 import BeautifulSoup as soup
+import requests
+import re
+from bs4 import BeautifulSoup
 
-my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+data = requests.get(
+    "https://www.radioshack.com/collections/featured-products/products/quantum-starter-component-kit").content
+soup = BeautifulSoup(data, "html.parser")
+span = soup.find("h1")
+title = span.text
 
+span = soup.find("p", {"class": "price_range"})
+price = span.text
 
-uClient = urlopen(my_url)
-page_html = uClient.read()
-uClient.close()
+span = soup.find("span", {"class": "vendor_wrapper"})
+vendor = span.text
 
-
-page_soup = soup(page_html, 'html.parser')
-
-
-containers = page_soup.findAll('div', {'class':'item-container'})
-
-filename = "products.csv"
-f = open(filename, 'w')
-
-headers = "Brands, Product, Shipping cost\n"
-
-f.write(headers)
-
-container = containers[1]
-
-for container in containers:
-    brand = container.div.div.a.img['title']
-    title_container = container.findAll('a', {'class':'item-title'})
-    product_name = title_container[0].text
-    ship_container = container.findAll('li', {'class':'price-ship'})
-    
-    shipping = ship_container[0].text.strip()
-
-    print("Brand is:" + brand)
-    print("Product:" + product_name)
-    print("Shipping cost:" + shipping)
-
-    f.write(brand + ',' + product_name.replace(',' , '|') + ',' + shipping + '\n')
-
-f.close()
+print("The Product: %s is from the following %s with a price of %s." % (title, vendor, price))
